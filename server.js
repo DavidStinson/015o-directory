925b93a025f38df97b9636e216479215c10d80e3
David Stinson <david.stinson.5@gmail.com>
2020-03-04 12:48:01 -0600

    heroku deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const passport = require("passport");
+const methodOverride = require("method-override")
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
@@ -20,6 +21,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(methodOverride('_method'));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
