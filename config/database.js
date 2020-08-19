35c9fb91eb72d25ed85c93b83b0eb2bb9751e66d
DavidStinson <mail@davidstinson.dev>
2020-08-19 07:33:54 -0500

    update deps

--- config/database.js
+++ config/database.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect('mongodb://localhost/015o', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
+});
+
+
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true
+// });
+
+const db = mongoose.connection;
+	
+db.on('connected', function() {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
